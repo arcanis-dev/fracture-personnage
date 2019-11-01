8459d1e37db7523cb59f0bbaae083d8e9d52b2d6
Arcanis <arcanis.dev@gmail.com>
2019-11-01 15:58:40 +0100

    Ajout du projet unity

---
+++ Fracture personnage/Assets/CutScript.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class CutScript : MonoBehaviour {
+    public Vector2 cutStart;
+    public Vector2 cutEnd;
+    public Vector2 mousePosition2D;
+    List<GameObject> Elements = new List<GameObject>();
+    private Camera cam;
+
+    private void Start() {
+        cam = Camera.main;
+    }
+
+    private void Update() {
+        this.Slice();
+
+
+        //Debug.Log(this.cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, this.cam.nearClipPlane)));
+    }
+
+    private void Slice() {
+        this.mousePosition2D = this.cam.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(0)) {
+            cutStart = mousePosition2D;
+            
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Debug.DrawLine(cutStart, mousePosition2D, Color.magenta);
+            RegisterElements();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            cutEnd = Input.mousePosition;
+            CutElements();
+            cutStart = Vector2.zero;
+            cutEnd = Vector2.zero;
+        }
+    }
+
+    private void RegisterElements() {
+        Debug.Log("Registering Elements");
+        var distance = Vector2.Distance(this.cutStart, this.mousePosition2D);
+        RaycastHit2D[] hits = Physics2D.RaycastAll(this.cutStart, this.mousePosition2D - this.cutStart, distance);
+
+        Debug.Log("hits length" + hits.Length);
+        
+        for (int i = 0; i < hits.Length; i++) {
+            Elements.Add(hits[i].transform.gameObject);
+            
+        }
+    }
+
+    private void CutElements() {
+        Debug.Log("Cutting Elements");
+        for (int i = 0; i < this.Elements.Count; i++) {
+            
+            //var elementPhysics = this.Elements[i].GetComponent<ElementPhysics>();
+        }
+
+    }
+    
+    
+}
---
+++ Fracture personnage/Assets/ElementPhysics.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementPhysics : MonoBehaviour {
+    public Rigidbody2D[] neighbors = new Rigidbody2D[4];
+    public List<Rigidbody2D> jointsRigidbodys;
+    public FixedJoint2D[] joints = new FixedJoint2D[4];
+
+    private Collider2D coll;
+    private Vector2 extents;
+
+    private void Awake() {
+        GetNeighboursRb();
+        CreateJoints();
+        coll = this.GetComponent<Collider2D>();
+        extents = this.coll.bounds.extents;
+    }
+    
+    private void GetNeighboursRb() {
+        var dir = new Vector2();
+        for (int i = 0; i < this.neighbors.Length; i++) {
+            switch (i) {
+                case 0:
+                    dir = Vector2.right;
+                    break;
+                case 1:
+                    dir = Vector2.down;
+                    break;
+                case 2:
+                    dir = Vector2.left;
+                    break;
+                case 3:
+                    dir = Vector2.up;
+                    break;
+            }
+
+            var hit = Physics2D.RaycastAll(transform.position, dir, dir.magnitude);
+            Debug.Log(hit.Length);
+            foreach (var t in hit) {
+                if (t.transform.CompareTag("Element")){
+                    if (t.transform.gameObject != this.gameObject) {
+                        this.neighbors[i] = t.rigidbody;
+                    }
+                }
+            }
+        }
+    }
+
+    private void CreateJoints() {
+        for (int i = 0; i < this.neighbors.Length; i++) {
+            if (this.neighbors[i] != null) {
+                this.joints[i] = gameObject.AddComponent<FixedJoint2D>();
+                this.joints[i].connectedBody = this.neighbors[i];
+            }
+        }
+    }
+}
